a6daae307c39181819ccde9d4c5895f7f19433c3
MikeGray31 <michaelkgray31@gmail.com>
2020-03-10 13:58:36 -0400

    project controller methods being added

--- src/main/java/com/example/demo/controllers/ProjectController.java
+++ src/main/java/com/example/demo/controllers/ProjectController.java
@@ -1,8 +1,13 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Project;
 import com.example.demo.services.ProjectServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 public class ProjectController {
@@ -12,5 +17,25 @@ public class ProjectController {
     @Autowired
     public ProjectController(ProjectServices projectServices) {
         this.projectServices = projectServices;
+    }
+
+    @PostMapping("/project/create")
+    public ResponseEntity<Project> createProject(@RequestBody Project newProject) {
+        return new ResponseEntity<>(this.projectServices.createProject(newProject), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/project/{id}")
+    public ResponseEntity<Project> findProject(@PathVariable Long id) {
+        return new ResponseEntity<>(this.projectServices.findProject(id), HttpStatus.OK);
+    }
+
+    @PutMapping("/project/{id}")
+    public ResponseEntity<Project> updateProject(@PathVariable Long id, @RequestBody Project newInfo){
+        return new ResponseEntity<>(this.projectServices.updateProject(id, newInfo), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/project/delete/{id}")
+    public ResponseEntity<Boolean> deleteProject(@PathVariable Long id){
+        return new ResponseEntity(this.projectServices.deleteProject(id), HttpStatus.OK);
     }
 }
--- src/main/java/com/example/demo/services/ProjectServices.java
+++ src/main/java/com/example/demo/services/ProjectServices.java
@@ -1,5 +1,6 @@
 package com.example.demo.services;
 
+import com.example.demo.models.Project;
 import com.example.demo.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +13,29 @@ public class ProjectServices {
     @Autowired
     public ProjectServices(ProjectRepository projectRepository) {
         this.projectRepository = projectRepository;
+    }
+
+    public Project createProject(Project newProject) {
+       return this.projectRepository.save(newProject);
+    }
+
+    public Project findProject(Long id) {
+        return this.projectRepository.findById(id).get();
+    }
+
+    public Project updateProject(Long id, Project newInfo) {
+        if(this.projectRepository.findById(id).isPresent()) {
+            newInfo.setProject_id(id);
+            return this.projectRepository.save(newInfo);
+        }
+        return null;
+    }
+
+    public Boolean deleteProject(Long id) {
+        if(this.projectRepository.existsById(id)){
+            this.projectRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
